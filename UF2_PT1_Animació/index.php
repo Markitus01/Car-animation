fc8092bd7feac4cda1a11adcf3f3af839b3694fe
Μαρκος01 <marksand2001@gmail.com>
2024-03-19 16:29:30 +0100

    Animacio

---
+++ UF2_PT1_Animació/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Animació</title>
+        <link rel="stylesheet" type="text/css" href="estil.css" />
+    </head>
+    <body>
+        <main>
+            <div id="astres"></div>
+
+            <div id="carro">
+                <?php include "./imgs/noia.svg"; ?>
+                
+                <div id="ruedinhas"></div>
+            </div>
+            
+            <div id="poste"></div>
+            <div id="arbres"></div>
+        </main>
+    </body>
+</html>
